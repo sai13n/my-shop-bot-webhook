9000d9c528a967a245ccf9f4094e43b76b64eff1
Sai Narraidu <45870607+sai13n@users.noreply.github.com>
2018-12-20 02:29:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ restService.post("/webhook", function(req, res) {
 
   const action = data.queryResult.action;
 
-  
-    let response = { fulfillmentText: "Webhook works. Action - " + action,}
+    if (action =="test_webhook") {
+    let response = { fulfillmentText: "Webhook works!",}
     res.json(response); 
-  
+    }
 
   /*switch(action) {
   case "detailed_order":
